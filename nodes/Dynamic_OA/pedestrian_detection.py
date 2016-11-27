e68f167d4f36c8b62d56a31f6a8d3b4fb6c017e6
Boris <boreinos@gmail.com>
2016-11-27 12:06:09 -0500

    Dynamic OA fully integrated with NATIVE and has launch file 'launch/dynamic_OA.launch'

---
+++ nodes/Dynamic_OA/pedestrian_detection.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+import math
+from std_msgs.msg import Bool
+
+#face_count = 0
+#body_count = 0
+
+#define Variables
+count = 0 #steady frame counter
+obstacle = False #publisher flag
+
+def callback_face(data):
+	global count, obstacle 
+	obstacle = False
+	if(data.data!= 0): 
+		count = count+1
+		if (count==15):
+			obstacle = True
+			count = 0
+			print '\tPedestrian detected!!!\n'
+
+	else:
+		#obstacle =False
+		count = 0
+#		print 'No pedestrian detected...'
+	face_pub = rospy.Publisher('pedestrian_found', Bool, queue_size = 1)
+	face_pub.publish(obstacle)
+
+def det_pedestrian():
+  rospy.init_node('pedestrian_found')
+  face_sub = rospy.Subscriber("/person_detection/nb_pedestrians", Int32, callback_face, queue_size = 1)
+  rospy.spin()
+
+
+if __name__ == "__main__":
+  det_pedestrian()
+else:
+  pass
